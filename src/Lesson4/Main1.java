77a0fd88ddd243be24830c125936d3970cf121e8
Cinnabon <anni_agust@mail.ru>
2021-05-31 15:38:58 +0300

    HW4

---
+++ src/Lesson4/Main1.java
@@ -0,0 +1,26 @@
+package Lesson4;
+
+import HW4.Phonebook;
+
+public class Main1 {
+    public static void main(String[] args) {
+        HW4.Phonebook phonebook = new Phonebook();
+
+        phonebook.add("Денежко", "9987451623");
+        phonebook.add("Кущ", "9975623145");
+        phonebook.add("Олейник", "9996658236");
+        phonebook.add("Фролов", "9825678943");
+        phonebook.add("Денежко", "9975621974");
+        System.out.println("-----");
+
+        System.out.println("Денежко");
+        System.out.println(phonebook.get("Денежко"));
+        System.out.println("Кущ");
+        System.out.println(phonebook.get("Кущ"));
+        System.out.println("Олейник");
+        System.out.println(phonebook.get("Олейник"));
+        System.out.println("Фролов");
+        System.out.println(phonebook.get("Фролов"));
+
+    }
+}
